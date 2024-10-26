5b5dbd95b89078f42ce512481558f60e23f0eec3
Louis Kuster <macbeth574@gmail.com>
2024-10-26 15:51:39 +0200

    New properties for Food class and Intialize method

--- Assets/Scripts/FoodCell.cs
+++ Assets/Scripts/FoodCell.cs
@@ -2,7 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FoodCell : Cell
+public class Food
 {
-    
+    public Rigidbody2D rigidBody;
+    public SpriteRenderer spriteRenderer;
+    public CircleCollider2D circleCollider;
+    public virtual void Initialize(GameObject prefab)
+    {
+        rigidBody = prefab.GetComponent<Rigidbody2D>();
+        spriteRenderer = prefab.GetComponent<SpriteRenderer>();
+        circleCollider = prefab.GetComponent<CircleCollider2D>();
+    }
 }
