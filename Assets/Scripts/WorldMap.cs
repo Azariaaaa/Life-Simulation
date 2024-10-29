c868b423af9f49df8f8d561ef0edb8a9d82a769e
Louis Kuster <macbeth574@gmail.com>
2024-10-29 21:22:04 +0100

    Added GetNewRandomPostion static method

--- Assets/Scripts/WorldMap.cs
+++ Assets/Scripts/WorldMap.cs
@@ -8,4 +8,9 @@ public static class WorldMap
     public static float maxXSpawnArea = 9f;
     public static float minYSpawnArea = 3.5f;
     public static float maxYSpawnArea = -3.5f;
+
+    public static Vector2 GetNewRandomPostion()
+    {
+        return new Vector2(Random.Range(minXSpawnArea, maxXSpawnArea), Random.Range(minYSpawnArea, maxYSpawnArea));
+    }
 }
